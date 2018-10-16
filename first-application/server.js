143a0e4f2d7e93902f03aaafe6dce5b7ad28cea7
James Houston <houstonj1@wit.edu>
2018-10-16 12:30:22 +0300

    Added the first application

---
+++ first-application/server.js
@@ -0,0 +1,17 @@
+/*
+    James Houston - 10/16/2018
+    Demo application with express
+    Changes to this file should only trigger builds on this application in TeamCity
+ */
+
+const express = require('express')
+const app = express()
+const PORT = 8080
+
+app.get('/', (request, response) => {
+  response.send('This works')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started, listening on port ${PORT}`)
+})
