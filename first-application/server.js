64baf29164a15a194b163c7b3aeddb7bfc547af3
James Houston <houstonj1@wit.edu>
2018-10-17 14:44:52 +0300

    More happiness

--- first-application/server.js
+++ first-application/server.js
@@ -9,7 +9,7 @@ const app = express()
 const PORT = 8080
 
 app.get('/', (request, response) => {
-  response.send('Hooray!')
+  response.send('Woohoo!')
 })
 
 app.listen(process.env.PORT || PORT, () => {
